3c5097547bdce6aa043b17887823a5c538438587
cassiels636 <35430596+cassiels636@users.noreply.github.com>
2018-05-24 23:30:11 -0400

    Fixed p2b

--- Project2/p2b.cpp
+++ Project2/p2b.cpp
@@ -22,13 +22,10 @@ int const NONE = -1;  // Used to represent a node that does not exist
 struct VertexProperties;
 struct EdgeProperties;
 
-typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
+typedef adjacency_list<vecS, vecS, undirectedS, VertexProperties, EdgeProperties> Graph;
 
-int greedyColoring(Graph &g, int numColors, int t);
+int greedyColoring(Graph &g, int numColors);
 void placeColor(Graph &g, Graph::vertex_iterator node, int &numConflicts, int numColors);
-//int exhaustiveColoring(Graph &g, int numColors, int t);
-//void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, clock_t startT, int t);
-//void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts);
 void printSolution(Graph &g, int numConflicts, string filePath_output);
 
 struct VertexProperties
@@ -89,7 +86,7 @@ int main()
 	// Read the name of the graph from the keyboard or
 	// hard code it here for testing.
 
-	fileName = "color12-3.input";
+	fileName = "color192-8.input";
 
 	//   cout << "Enter filename" << endl;
 	//   cin >> fileName;
@@ -114,9 +111,8 @@ int main()
 		cout << "Num edges: " << num_edges(g) << endl;
 		cout << endl;
 
-		numConflicts = greedyColoring(g, numColors, 600);
-//		numConflicts = exhaustiveColoring(g, numColors, 600);
-		string output = "/Users/Cassie/source/color12-3.output";
+		numConflicts = greedyColoring(g, numColors);
+		string output = "/Users/Cassie/source/color192-8.output";
 		printSolution(g, numConflicts, output);
 		system("pause");
 
@@ -131,9 +127,7 @@ int main()
 	}
 }
 
-
-int greedyColoring(Graph &g, int numColors, int t) {
-	clock_t startTime = clock();
+int greedyColoring(Graph &g, int numColors) {
 	int numConflicts = 0;
 	//gets start and end vertex iterators (which allow you to access the vertex)
 	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
@@ -155,103 +149,6 @@ int greedyColoring(Graph &g, int numColors, int t) {
 }
 
 
-/*
-//Exhaustive calculates the least number of conflicts in a graph coloring given the
-//number of colors and a graph. Exits function if the time taken exceeds t.
-int exhaustiveColoring(Graph &g, int numColors, int t) {
-	clock_t startTime = clock();
-
-	//gets start and end vertex iterators (which allow you to access the vertex)
-	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
-
-	Graph::vertex_iterator firstNode = vItrRange.first;
-	Graph::vertex_iterator lastNode = vItrRange.second;
-
-	//assign all vertex colors to 0, meaning lack of color
-	for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
-		g[*vItr].color = 0;
-	}
-
-	Graph bestG = g;
-	int bestGConflicts = num_edges(g);
-
-	//Calculates least number of conflicts recursively
-	RGT(g, bestG, bestGConflicts, numColors, firstNode, startTime, t);
-
-	//Returns least number of conflicts and the graph coloring that corresponds to it
-	g = Graph(bestG);
-	return bestGConflicts;
-}
-
-//Recursively tests every coloring combination of a graph and tracks the combination with the least number of conflicts.
-//Exits the function if the time taken exceeds t.
-void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, clock_t startT, int t) {
-	pair<Graph::vertex_iterator, Graph::vertex_iterator> range = vertices(g);
-
-	//Exit function if time is exceeded
-	if (((float)(clock() - startT) / CLOCKS_PER_SEC) >= t) {
-		return;
-	}
-	for (int x = 1; x <= numColors; x++) {
-		g[*start].color = x; //set color of graph for start node to x
-
-							 //check if start is the last node
-		if ((start + 1) >= range.second) {
-			checkConflicts(g, bestG, bestGConflicts);
-		}
-		//otherwise call function recursively
-		else {
-			RGT(g, bestG, bestGConflicts, numColors, start + 1, startT, t);
-		}
-
-		//Exit function if time is exceeded
-		if (((float)(clock() - startT) / CLOCKS_PER_SEC) >= t) {
-			return;
-		}
-	}
-
-	//Reset node to 0 meaning lack of color
-	g[*start].color = 0;
-	return;
-}
-
-//Compares a graph coloring to the current best graph coloring and replaces it
-//if it has less conflicts.
-void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts) {
-	int conflicts = 0, color1, color2;
-	pair<Graph::edge_iterator, Graph::edge_iterator> eItrRange = edges(g);
-
-	//Adds up all conflicts
-	for (Graph::edge_iterator eItr = eItrRange.first; eItr != eItrRange.second; ++eItr) {
-		color1 = g[target(*eItr, g)].color;
-		color2 = g[source(*eItr, g)].color;
-
-		if (color1 == color2) {
-			conflicts++;
-		}
-	}
-
-	//Compares conflicts to current least number of conflicts
-	if (conflicts < bestGConflicts) {
-		bestGConflicts = conflicts;
-		bestG = Graph(g);
-	}
-}*/
-
-//Prints the best graph coloring calculated
-void printSolution(Graph &g, int numConflicts, string filePath_output) {
-	ofstream myfile;
-	myfile.open(filePath_output.c_str());
-
-	myfile << "Total Conflicts: " << numConflicts << endl;
-
-	for (int counter = 0; counter < num_vertices(g); counter++) {
-		myfile << counter << ": " << g[counter].color << endl;
-	}
-
-	myfile.close();
-}
-
 void placeColor(Graph &g, Graph::vertex_iterator node, int &numConflicts, const int numColors) {
 	vector<int> conflicts;
 	conflicts.resize(numColors + 1);
@@ -260,6 +157,7 @@ void placeColor(Graph &g, Graph::vertex_iterator node, int &numConflicts, const 
 	// list of nodes adjacent to node v
 	pair<Graph::adjacency_iterator, Graph::adjacency_iterator> adjRange = adjacent_vertices(*node, g);
 
+	// Adjacent node list is incorrect for our purposes
 	// Loop over adjacent nodes in the graph
 	for (Graph::adjacency_iterator Itr = adjRange.first; Itr != adjRange.second; ++Itr) {
 		conflicts[g[*Itr].color] += 1;
@@ -286,4 +184,18 @@ void placeColor(Graph &g, Graph::vertex_iterator node, int &numConflicts, const 
 	}
 
 	return;
+}
+
+//Prints the best graph coloring calculated
+void printSolution(Graph &g, int numConflicts, string filePath_output) {
+	ofstream myfile;
+	myfile.open(filePath_output.c_str());
+
+	myfile << "Total Conflicts: " << numConflicts << endl;
+
+	for (int counter = 0; counter < num_vertices(g); counter++) {
+		myfile << counter << ": " << g[counter].color << endl;
+	}
+
+	myfile.close();
 }
