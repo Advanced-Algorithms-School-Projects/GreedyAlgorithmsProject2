49b4e5ccd9ad468e2a530e7da7f113bd4c0946ad
Patrick Hanbury <patrickhanbury1@gmail.com>
2018-05-24 23:01:44 -0400

    Started debugging graphColouring problem. Problem is color of all adjacent nodes are 0 - even though graph node colors are being updated. Problem is likely in lines 195 to 202 of pb2.cpp

--- Project2/p2b.cpp
+++ Project2/p2b.cpp
@@ -25,7 +25,7 @@ struct EdgeProperties;
 typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
 
 int greedyColoring(Graph &g, int numColors, int t);
-void placeColor(Graph &g, Graph::vertex_iterator node, int &numConflicts, int numColors);
+void placeColor(Graph &g, Graph::vertex_iterator &node, int &numConflicts, int numColors);
 //int exhaustiveColoring(Graph &g, int numColors, int t);
 //void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, clock_t startT, int t);
 //void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts);
@@ -88,8 +88,8 @@ int main()
 
 	// Read the name of the graph from the keyboard or
 	// hard code it here for testing.
-
-	fileName = "color12-3.input";
+	string inputName = "12-3";
+	fileName = "color" + inputName + ".input"; 
 
 	//   cout << "Enter filename" << endl;
 	//   cin >> fileName;
@@ -116,7 +116,7 @@ int main()
 
 		numConflicts = greedyColoring(g, numColors, 600);
 //		numConflicts = exhaustiveColoring(g, numColors, 600);
-		string output = "/Users/Cassie/source/color12-3.output";
+		string output = "/Users/patri/Desktop/Proj2-Git/GreedyAlgorithmsProject2/Output Files/color" + inputName + ".output";
 		printSolution(g, numConflicts, output);
 		system("pause");
 
@@ -130,7 +130,6 @@ int main()
 		cout << ex.what() << endl; exit(1);
 	}
 }
-
 
 int greedyColoring(Graph &g, int numColors, int t) {
 	clock_t startTime = clock();
@@ -149,94 +148,17 @@ int greedyColoring(Graph &g, int numColors, int t) {
 	//assign all vertex colors to 0, meaning lack of color
 	for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
 		placeColor(g, vItr, numConflicts, numColors);
+		
+		int counter = 0;
+		cout << endl << endl << endl << "New Graph:" << endl;
+		for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
+			cout << "node number: " << counter << "    color = " << g[*vItr].color << endl;
+			counter++;
+		}
 	}
 
 	return numConflicts;
 }
-
-
-/*
-//Exhaustive calculates the least number of conflicts in a graph coloring given the
-//number of colors and a graph. Exits function if the time taken exceeds t.
-int exhaustiveColoring(Graph &g, int numColors, int t) {
-	clock_t startTime = clock();
-
-	//gets start and end vertex iterators (which allow you to access the vertex)
-	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
-
-	Graph::vertex_iterator firstNode = vItrRange.first;
-	Graph::vertex_iterator lastNode = vItrRange.second;
-
-	//assign all vertex colors to 0, meaning lack of color
-	for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
-		g[*vItr].color = 0;
-	}
-
-	Graph bestG = g;
-	int bestGConflicts = num_edges(g);
-
-	//Calculates least number of conflicts recursively
-	RGT(g, bestG, bestGConflicts, numColors, firstNode, startTime, t);
-
-	//Returns least number of conflicts and the graph coloring that corresponds to it
-	g = Graph(bestG);
-	return bestGConflicts;
-}
-
-//Recursively tests every coloring combination of a graph and tracks the combination with the least number of conflicts.
-//Exits the function if the time taken exceeds t.
-void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, clock_t startT, int t) {
-	pair<Graph::vertex_iterator, Graph::vertex_iterator> range = vertices(g);
-
-	//Exit function if time is exceeded
-	if (((float)(clock() - startT) / CLOCKS_PER_SEC) >= t) {
-		return;
-	}
-	for (int x = 1; x <= numColors; x++) {
-		g[*start].color = x; //set color of graph for start node to x
-
-							 //check if start is the last node
-		if ((start + 1) >= range.second) {
-			checkConflicts(g, bestG, bestGConflicts);
-		}
-		//otherwise call function recursively
-		else {
-			RGT(g, bestG, bestGConflicts, numColors, start + 1, startT, t);
-		}
-
-		//Exit function if time is exceeded
-		if (((float)(clock() - startT) / CLOCKS_PER_SEC) >= t) {
-			return;
-		}
-	}
-
-	//Reset node to 0 meaning lack of color
-	g[*start].color = 0;
-	return;
-}
-
-//Compares a graph coloring to the current best graph coloring and replaces it
-//if it has less conflicts.
-void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts) {
-	int conflicts = 0, color1, color2;
-	pair<Graph::edge_iterator, Graph::edge_iterator> eItrRange = edges(g);
-
-	//Adds up all conflicts
-	for (Graph::edge_iterator eItr = eItrRange.first; eItr != eItrRange.second; ++eItr) {
-		color1 = g[target(*eItr, g)].color;
-		color2 = g[source(*eItr, g)].color;
-
-		if (color1 == color2) {
-			conflicts++;
-		}
-	}
-
-	//Compares conflicts to current least number of conflicts
-	if (conflicts < bestGConflicts) {
-		bestGConflicts = conflicts;
-		bestG = Graph(g);
-	}
-}*/
 
 //Prints the best graph coloring calculated
 void printSolution(Graph &g, int numConflicts, string filePath_output) {
@@ -246,39 +168,62 @@ void printSolution(Graph &g, int numConflicts, string filePath_output) {
 	myfile << "Total Conflicts: " << numConflicts << endl;
 
 	for (int counter = 0; counter < num_vertices(g); counter++) {
-		myfile << counter << ": " << g[counter].color << endl;
+		
+	}
+
+	//gets start and end vertex iterators (which allow you to access the vertex)
+	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
+	Graph::vertex_iterator firstNode = vItrRange.first;
+	Graph::vertex_iterator lastNode = vItrRange.second;
+
+	int counter = 0;
+	//set all colors to zero
+	for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
+		myfile << counter << ": " << g[*vItr].color << endl;
+		counter++;
 	}
 
 	myfile.close();
 }
 
-void placeColor(Graph &g, Graph::vertex_iterator node, int &numConflicts, const int numColors) {
+void placeColor(Graph &g, Graph::vertex_iterator &node, int &numConflicts, const int numColors) {
 	vector<int> conflicts;
-	conflicts.resize(numColors + 1);
+	conflicts.resize(numColors + 1,0);
 
 	// Get a pair containing iterators pointing to the beginning and end of the
 	// list of nodes adjacent to node v
 	pair<Graph::adjacency_iterator, Graph::adjacency_iterator> adjRange = adjacent_vertices(*node, g);
 
 	// Loop over adjacent nodes in the graph
-	for (Graph::adjacency_iterator Itr = adjRange.first; Itr != adjRange.second; ++Itr) {
-		conflicts[g[*Itr].color] += 1;
+	for (Graph::adjacency_iterator Itr = adjRange.first; Itr != adjRange.second; ++Itr) { //for some reason g[*Itr].color is always 0
+		cout << endl << "Updating Conflicts Vector: current[" << *Itr << "]=" << conflicts[g[*Itr].color] << " color=" << g[*Itr].color << endl;
+		conflicts[g[*Itr].color] = conflicts[g[*Itr].color] + 1;
+		cout << "conflicts color: " << g[*Itr].color << " and #conflicts " << conflicts[g[*Itr].color] << endl;
 	}
 
+	cout << "Conflicts[0] = " << conflicts[0] << endl;
+
 	int minConflicts = numColors + 1;
+	cout << "minConflicts = " << minConflicts << endl;
 	int color; //variable to hold the color number
 
 	//find minimum of conflicts vector
 	//assign color corresponding to minimum number of conflicts
 	for (int i = 1; i <= numColors; i++) {
+		cout << "FOR LOOP:   i=" << i << "    numColors = " << numColors;
 		if (conflicts[i] < minConflicts) {
+			cout << "   TRUE" << "  Conflicts[" << i << "] = " << conflicts[i] << endl;
 			minConflicts = conflicts[i];
 			color = i;
+		}
+		else {
+			cout << "   FALSE" << endl;
 		}
 	}
 
 	//place color
 	g[*node].color = color;
+	cout << "Does node color get updated?" << g[*node].color << endl;
 
 	//check if this node contains the max conflicts
 	if (minConflicts > numConflicts) {
